8f1c3bd84cfdd91e331f93981e95ed08c4188874
Pakorn Sangpeth <camel4882@gmail.com>
2025-11-15 13:06:30 +0700

    remove timeout context

--- pkg/transport/conn.go
+++ pkg/transport/conn.go
@@ -12,7 +12,6 @@ func NewConn(ctx context.Context, target string) (*grpc.ClientConn, error) {
 	if len(target) == 0 {
 		return nil, errors.New("target is null, it should be a remote endpoint or a unix domain socket")
 	}
-
 	conn, err := grpc.DialContext(ctx, target, grpc.WithCredentialsBundle(insecure.NewBundle()), grpc.WithBlock())
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to dial %s", target)
